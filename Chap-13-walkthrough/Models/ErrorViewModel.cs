e912e725d3d2ce1e95645744689ab4b375caaf61
Kristen Thoma <kristenthoma@gmail.com>
2021-07-19 22:15:35 -0400

    Add project files.

---
+++ Chap-13-walkthrough/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Chap_13_walkthrough.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
